09a88ebc09ad971e69419f4c488576b8b3e8cf1d
cuongnm <motnetmc@gmail.com>
2017-11-12 22:43:34 +0700

    update

--- src/db.js
+++ src/db.js
@@ -7,20 +7,22 @@ import mysql from 'mysql'
   database: "shippy_local"
 });*/
 
+// let connection = mysql.createConnection({
+//   host: 'shippy.mysql.database.azure.com',
+//   user: 'cuongnm@shippy',
+//   password: 'Cuong1821995',
+//   database: 'shippy',
+//   port: 3306,
+//   ssl: true
+// });
+
 let connection = mysql.createConnection({
-  host: 'shippy.mysql.database.azure.com',
-  user: 'cuongnm@shippy',
-  password: 'Cuong1821995',
-  database: 'shippy',
+  host: "shippy.cum65ahmr9e4.us-west-2.rds.amazonaws.com",
+  user: "shippy",
+  password: "12345678",
+  database: "Shippy",
   port: 3306,
   ssl: true
 });
-
-// let connection = mysql.createConnection({
-//   host: "shippy.cum65ahmr9e4.us-west-2.rds.amazonaws.com",
-//   user: "shippy",
-//   password: "12345678",
-//   database: "Shippy"
-// });
 
 module.exports = connection;
