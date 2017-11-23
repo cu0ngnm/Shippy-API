d1d5b183643f2c873c63068e74d31a134da73589
cuongnm <motnetmc@gmail.com>
2017-11-23 23:45:57 +0700

    add gg cloud

--- src/utilities/constant.js
+++ src/utilities/constant.js
@@ -4,12 +4,15 @@ export default {
   TOKEN_SECRET: 'y2g0q7wCRZmbiweSJxlz',
   TOKENTIME: 60*60*24*30, //30 days
 
-  //FCM
+  //Firebase cloud message
   FCM_SERVER_KEY: 'AAAA7TJoeFg:APA91bFdwgY-63pNSAerTiNC-pDcyC2M0MWNmuez1g6TmLVbE1dqYiuPAlJTJeEeP_i7rR5Ndnp5un7EvAjePvbb24Z47cgmivQ8dCy2J-KDrFc12e-yi_JhuHH4pxPVgRwskxJwP8SV',
 
   //order status
+  FINISHED_ORDER: 0,
   WAITTING_ORDER: 1,
   RECIEVED_ORDER: 2,
   CANCELED_ORDER: 3,
 
+  //google-cloud storage
+  BUCKET_NAME: 'shippy_1802'
 }
